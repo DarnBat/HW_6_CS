5035fdc6d4a7c096a1d50fb089ff47f1730658b8
Vladislav.M <lidpid@gmail.com>
2023-01-12 18:29:39 +0600

    task 41 completed

---
+++ Task_41/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите числа через пробел: ");
+int count = 0;
+int[] array = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+for(int i = 0; i < array.Length; i++){
+    if(array[i] > 0) count++;
+}
+Console.WriteLine(count);
